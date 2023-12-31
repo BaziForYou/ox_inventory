f2ac25db47527e1c64d1395ef20ec09bd756c4e4
RazerGhost <IRazerIGhost@gmail.com>
2023-12-31 23:15:51 +0100

    added base V2.37.4

---
+++ modules/bridge/nd/client.lua
@@ -0,0 +1,42 @@
+if not lib.checkDependency('ND_Core', '2.0.0', true) then return end
+
+lib.load('@ND_Core.init')
+
+RegisterNetEvent("ND:characterUnloaded", client.onLogout)
+
+local function reorderGroups(groups)
+    groups = groups or {}
+    for group, info in pairs(groups) do
+        groups[group] = info.rank
+    end
+    return groups
+end
+
+SetTimeout(500, function()
+	local player = NDCore.getPlayer()
+    if not player then return end
+    local groups = reorderGroups(player.groups)
+    OnPlayerData("groups", groups)
+end)
+
+RegisterNetEvent("ND:setCharacter", function(character)
+    local groups = reorderGroups(character.groups)
+    OnPlayerData("groups", groups)
+end)
+
+RegisterNetEvent("ND:updateCharacter", function(character)
+    PlayerData.dead = character.metadata.dead
+    OnPlayerData("dead", PlayerData.dead)
+end)
+
+---@diagnostic disable-next-line: duplicate-set-field
+function client.setPlayerStatus(values)
+    if GetResourceState("ND_Status") ~= "started" then return end
+	for name, value in pairs(values) do
+        if value == 0 then
+            exports["ND_Status"]:setStatus(name, value)
+        else
+            exports["ND_Status"]:changeStatus(name, value)
+        end
+    end
+end
